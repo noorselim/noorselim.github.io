76645f569e17e13004b17a1e8d3e8a16145d5955
noorselim <selin810@newschool.edu>
2022-03-08 00:45:31 -0500

    upload stories as networks

---
+++ stories-as-networks/script.js
@@ -0,0 +1,15 @@
+// let body = document.body;
+// let textReplacement = document.querySelector(".text8")
+
+// function swapText() {
+//     textReplacement.classList.toggle("show");
+//     button.classList.toggle("hide");
+// }
+
+// let button = document.querySelector(".symbols");
+// button.addEventListener("mouseover", swapText);
+// textReplacement.addEventListener("mouseover", swapText);
+
+document.getElementById("enter").onclick = function () {
+    location.href = "question-mark.html";
+};
